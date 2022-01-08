1b6d442f40138f01f062c206256643637ef6e00b
Christopher A. Flores <kershingf@gmail.com>
2022-01-08 08:18:26 -0600

    remove webpack and replace frontend with nuxt

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-// import tailwindcss from 'tailwindcss'
-
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
-  }
-};
--- src/js/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const SERVER = window.location.hostname === 'localhost' ? 'http://localhost:9000/api' : ''
-
-axios.defaults.baseURL = SERVER
-
-export default axios
